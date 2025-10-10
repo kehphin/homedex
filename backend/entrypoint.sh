69c2a986bf907b234f323d4da88e04bb0e398801
Kevin Yang <yang.k@me.com>
2025-10-09 21:21:58 -0500

    typo

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,7 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
-&2 echo "Postgres is up - continuing"
+>&2 echo "Postgres is up - continuing"
 
 # Verify database connection and print version
 echo "Verifying database connection..."
